07288803104a55766e3367bc8db2cb3ab243091e
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-09-17 20:53:22 -0300

    Create getCOmputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    let n = Math.trunc(Math.random() * 3 + 1);
+    return n == 1 ? "rock" : n == 2 ? "paper" : "scissors"
+}
+console.log(getComputerChoice())
