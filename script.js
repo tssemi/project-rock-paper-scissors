51c7ca5d09833a7b2f56f80ff4fb87960fcf5d75
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-09-18 18:39:08 -0300

    Create a pseudocode

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-console.log("Hello World");
+//A FUNCTION gives VARIABLE a random string between rock, paper and scissors
+//The player enters another string into a VARIABLE using PROMPT (rock, paper ora scissors)
+//Both VARIABLES are compared in a FUNCTION
+//Whoever wins earns one point per round
+//A FUNCTION will keep track of the players score.
 
 function getComputerChoice() {
     let n = Math.trunc(Math.random() * 3 + 1);
     return n == 1 ? "rock" : n == 2 ? "paper" : "scissors"
 }
-console.log(getComputerChoice())
+
