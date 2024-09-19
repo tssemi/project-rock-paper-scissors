8c79f7a595c8b363256d225f2f37812a526346b4
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-09-19 19:27:01 -0300

    Create a function to compare the variables

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//Both VARIABLES are compared in a FUNCTION
 //Whoever wins earns one point per round
 //A FUNCTION will keep track of the players score.
 
@@ -15,6 +14,44 @@ function getHumanChoice() {
     return check(n)
 }
 
-let check = (n) => n == 1 ? "rock" : n == 2 ? "paper" : "scissors"
+let check = (n) => n == 1 ? "rock" : n == 2 ? "paper" : "scissors";
+
+let computerScore = 0;
+let humanScore = 0;
+
+//Both VARIABLES are compared in a FUNCTION
+function playRound(humChoice, comChoice) {
+    humChoice = getHumanChoice();
+    console.log(humChoice)
+    comChoice = getComputerChoice();
+    console.log(comChoice)
+    switch (humChoice.toLowerCase()) {
+        case "rock":
+            if (comChoice == "rock") {
+                console.log("It's a tie.");
+            } else if (comChoice == "paper") {
+                console.log("You lose...");
+                computerScore += 1;
+                console.log(computerScore + " comp")
+                console.log(humanScore + " human")
+
+            } else {
+                console.log("You win!");
+                humanScore += 1;
+                console.log(computerScore + " comp")
+                console.log(humanScore + " human")
+            }
+            break;
+        case "paper":
+
+            break;
+        case "scissors":
+
+            break;
+    
+        default:
+            break;
+    }
+}
 
 
