393ecdab27f14ba6cf5b711cd4217233c151d1fd
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-09-19 18:32:54 -0300

    Create two new functions

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-//A FUNCTION gives VARIABLE a random string between rock, paper and scissors
-//The player enters another string into a VARIABLE using PROMPT (rock, paper ora scissors)
 //Both VARIABLES are compared in a FUNCTION
 //Whoever wins earns one point per round
 //A FUNCTION will keep track of the players score.
 
+
+//A FUNCTION gives VARIABLE a random string between rock, paper and scissors
 function getComputerChoice() {
     let n = Math.trunc(Math.random() * 3 + 1);
-    return n == 1 ? "rock" : n == 2 ? "paper" : "scissors"
+    return check(n)
 }
+
+//The player enters another string into a VARIABLE using PROMPT (rock, paper ora scissors)
+function getHumanChoice() {
+    let n = prompt("Let's play! Choose one... Rock(1), Paper(2) or Scissors(3 or other)?", );
+    return check(n)
+}
+
+let check = (n) => n == 1 ? "rock" : n == 2 ? "paper" : "scissors"
+
 
